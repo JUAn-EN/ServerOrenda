87dead08b786130fd0eb3eb4687c1e5dede4d563
juanesteban22 <jestenieto@gmail.com>
2024-06-25 08:58:53 -0500

    cambios nuevos en el back

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const routes = require('./Routes/Routes');
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://singular-muffin-717fae.netlify.app/',
+    origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
@@ -23,6 +23,18 @@ if (!fs.existsSync(uploadDir)) {
 } else {
     console.log('La carpeta de carga ya existe:', uploadDir);
 }
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', "default-src 'none'; img-src 'self'; object-src 'self' uploads/;");
+    next();
+});
+
+// Después de tus rutas
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('¡Algo salió mal!');
+});
+
 
 
 module.exports = app;
